0e6c3ff2890d5ffcc08873d79b74729431cd2cea
Francisco Javier Zaragoza Zepeda <francisco.zaragozaza@udlap.mx>
2017-09-05 01:33:05 -0500

    Add matrix

--- HW4/Controller/SplitFile.cs
+++ HW4/Controller/SplitFile.cs
@@ -8,25 +8,47 @@ namespace HW4.Controller
 {
     class SplitFile
     {
-        String colection = "";
-        String [] splittedCollection1;
-        
+        String collection = "";
+        String[] splittedCollection1;
+
         public SplitFile(String aColection)
         {
-            this.colection = aColection;
+            this.collection = aColection;
         }
 
-        public void SplitLISA() 
+        public void SplitLISA()
         {
-            String[] arregloAux = new String[splittedCollection1.Count()];
-            splittedCollection1 = colection.Split(new[] { "********************************************" }, StringSplitOptions.None);
+            /**
+             * array[]
+             * arrayAux[]
+             * Matrix [][]
+             * 
+             * split se hace con substring y con FindIndex
+             * loop que corre la cantidad de splits que haya de acuerdo a la primera condición (será la cantidad de documentos en el texto)
+             * 
+             * 
+             **/
 
-            for(int i = 0; i<splittedCollection1.Count(); i++)
+            // Declaración de variables:
+            String indexSplitColection;
+            String indexSplitTitle;
+
+            indexSplitColection = "********************************************";
+            splittedCollection1 = collection.Split(new[] { indexSplitColection }, StringSplitOptions.None);
+            String[] arregloAux = new String[splittedCollection1.Count()];
+            //Se crea un arreglo de arreglos del tamaño necesario.
+            String[,] matrix = new String[splittedCollection1.Count(), 3];
+
+            for (int i = 0; i < splittedCollection1.Count(); i++)
             {
+                matrix[i, 0] = "ID";
+                matrix[i, 1] = "Título";
+                matrix[i, 2] = "Texto";
                 // Vamos a ocupar substring e index of para obtener el titulo, id y texto separados de splittedCollection1 :3 :*
 
             }
-            //Eliminar arreglos de memoria
+            indexSplitTitle = "\r\n";
+
         }
 
     }
